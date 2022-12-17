66bf6321992786f83e72f4344478d40f94b68f3f
Utsav <utsavdeep01@gmail.com>
2022-12-17 12:32:29 +0530

    Basic Functionality

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 ## To Do
 
-- [ ] Launch ChromeDriver
-- [ ] Search for google.com
-- [ ] Get/Read User input on Job title and Company Name
-- [ ] Enter the search string - site:in.linkedin.com/in intitle: Job title and/or Company name
-- [ ] Get the results title and profile link recursively
+- [x] Launch ChromeDriver
+- [x] Search for google.com
+- [x] Get/Read User input on Job title and Company Name
+- [x] Enter the search string - site:in.linkedin.com/in intitle: Job title and/or Company name
+- [x] Get the results title and profile link recursively
+- [ ] Seprate the code into POM
+- [ ] Handle Captcha during search
+- [ ] Write Data into CSV
--- main.py
+++ main.py
@@ -1,5 +1,41 @@
 from selenium import webdriver
+from time import sleep
 
-browser = webdriver.Firefox()
-browser.get("http://selenium.dev/")
-browser.quit()
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+company_name = input("Enter a company name: ")
+job_title = input("Enter job title: ")
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+sleep(3)
+
+search_input = driver.find_element(By.XPATH, "//input[@title='Search']")
+search_input.send_keys(
+    f"site:in.linkedin.com/in intitle:{job_title} {company_name}", Keys.ENTER
+)
+try:
+    captcha = driver.find_element(By.XPATH, "//*[contains(@id, 'recaptcha')]")
+    if captcha.is_displayed():
+        sleep(120)
+except:
+    print("captcha not displayed")
+next_btn_displayed = True
+while next_btn_displayed:
+    titles = driver.find_elements(By.XPATH, "//h3")
+    links = driver.find_elements(By.XPATH, "//h3/parent::a")
+    for (title, link) in zip(titles, links):
+        print(title.text)
+        print(link.get_attribute("href"))
+    try:
+        next_button = driver.find_element(
+            By.XPATH, "//span[contains(text(), 'Next')]/parent::a"
+        )
+        next_button.click()
+    except:
+        next_btn_displayed = False
+    sleep(10)
+
+driver.quit()
